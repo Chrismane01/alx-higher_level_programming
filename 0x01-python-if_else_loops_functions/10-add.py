0d07b3ea0e6ac4c3d4e0ed2212832ee714809e24
Chrismane01 <christianigwebuike30@gmail.com>
2022-12-01 21:52:53 -0800

    first commit

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Igwebuike Christian Chukwudi
+def add(a, b):
+"""Return the addition of a and b."""
+return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author - Igwebuike Christian Chukwudi
+def pow(a, b):
+return (a ** b)
