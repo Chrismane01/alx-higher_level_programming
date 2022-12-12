cdef4c70612b8c65646d08560db0062eec27742c
Chrismane01 <christianigwebuike30@gmail.com>
2022-12-12 00:46:59 -0800

    first commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# Author - Igwebuike Christian Chukwudi
 def islower(c):
-"""Function checks for lowercase characters."""
-if ord(c) >= 97 and ord(c) <= 122:
-return True
-else:
-return False
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
