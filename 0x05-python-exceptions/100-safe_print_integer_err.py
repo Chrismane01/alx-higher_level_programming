f2b0abe33e2f2fd740504db1677b4f97d6c5e94a
Chrismane01 <christianigwebuike30@gmail.com>
2022-12-20 03:26:24 -0800

    first commit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+
+import sys
+
+
+
+
+
 def safe_print_integer_err(value):
-    import sys
+
     try:
+
         print("{:d}".format(value))
+
         return True
-    except Exception as e:
+
+    except (ValueError, TypeError) as e:
+
         print("Exception: {}".format(e), file=sys.stderr)
+
         return False
-    
