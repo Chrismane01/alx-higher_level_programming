fc5ee6512dd6726eb750e57d0695aefb7ca014a2
Chrismane01 <christianigwebuike30@gmail.com>
2023-01-10 07:36:11 -0800

    first commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,10 +14,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file = "add_item.json"
 json_list = []
 
-if os.path.exists(file):    
+if os.path.exists(file):
     json_list = load_from_json_file(file)
 
-for i in range(1, len(sys.argv)):    
+for i in range(1, len(sys.argv)):
     json_list.append(sys.argv[i])
 
 save_to_json_file(json_list, file)
