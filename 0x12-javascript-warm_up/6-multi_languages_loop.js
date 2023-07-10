22b4972c170868a460ed10d25aec8d4f73c6c4eb
Chrismane01 <christianigwebuike30@gmail.com>
2023-07-10 00:55:23 -0700

    first

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-let  myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-len = myArray.length;
-let i;
 
-for (i = 0; i < len; i++) {
-    console.log(myArray[i]);
+const myStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let idx = 0;
+
+while (idx < 3) {
+  console.log(myStr[idx]);
+  idx++;
 }
