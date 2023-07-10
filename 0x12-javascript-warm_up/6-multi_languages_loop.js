942f95b42c343f3207ce956bec1c2c4f51ad56cb
Chrismane01 <christianigwebuike30@gmail.com>
2023-07-10 00:37:54 -0700

    first

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let  myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+len = myArray.length;
+let i;
+
+for (i = 0; i < len; i++) {
+    console.log(myArray[i]);
+}
