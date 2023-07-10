b8a02a018443e045c9bf4e564e1f18bb5bb8af7f
Chrismane01 <christianigwebuike30@gmail.com>
2023-07-09 17:36:03 -0700

    first

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length == 2){
+if (process.argv.length === 2){
     console.log('No arguement');
-}else if (process.argv.length == 3){
+}else if (process.argv.length === 3){
     console.log('Arguement found');
 }else{
     console.log('Arguements found');
