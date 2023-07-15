d7ce881bfc2c498cba707810d2d5b45f181bc7c2
Chrismane01 <christianigwebuike30@gmail.com>
2023-07-15 12:47:46 -0700

    first

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -13,3 +13,5 @@ module.exports = class Rectangle {
     }
   }
 }
+
+module.exports = Rectangle;
